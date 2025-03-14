264ac1a6841fe7b9ebbeb7f2a4cfda5c46c52c91
noeul <kee12355@gmail.com>
2025-03-14 22:44:21 +0900

    Update 별 찍기 － 1.py

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -3,10 +3,11 @@ import sys
 
 n = int(sys.stdin.readline().strip())
 
-for i in range(n):
-    print('*' * (i+1))
-
-
+for i in range(number):
+    for j in range(i+1):
+        print("*", end="")
+    print("")            
+    
     
 
 
