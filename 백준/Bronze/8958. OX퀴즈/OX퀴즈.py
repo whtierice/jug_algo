9d3459f110302e001bf77388afb7b3d246afe18b
noeul <kee12355@gmail.com>
2025-03-14 19:35:13 +0900

    [Bronze II] Title: OX퀴즈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+score = 1
+sum = 0
+
+for i in range(n):
+    a = list(sys.stdin.readline().strip())
+
+    for i in range(len(a)):
+        if a[i] == 'O':
+            if i > 0 and (a[i] == a[i-1]):
+                score += 1
+                sum += score
+            else:
+                sum += score
+        else:
+            score = 1
+
+    
+    print(sum)
+
+    score = 1
+    sum = 0
