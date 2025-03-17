4aa676eb69c0aa491932de77184e077d83b5d978
noeul <kee12355@gmail.com>
2025-03-17 09:31:37 +0900

    Update 수 정렬하기 3.py

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,17 +1,13 @@
 import sys
-input = sys.stdin.readline
 
-#계수정렬 활용
-n = int(input())
-arr = [0] * (10000 + 1) # 입력값이 10000개까지 주어지니 10000 + 1개의 리스트 선언
+n = int(sys.stdin.readline().strip())
 
-#각 입력값에 해당하는 인덱스의 값 증가
-for _ in range(n):
-    arr[int(input())] += 1
-  
-#arr에 기록된 정보 확인
-for i in range(len(arr)):
-    if arr[i] != 0: #0이 아닌 데이터, 즉 입력받은 데이터들을 출력
-        for _ in range(arr[i]):
+s_array = [0] * 10001
+
+for i in range(n):
+    s_array[int(sys.stdin.readline().strip())] += 1
+
+for i in range(1,10001):
+    if s_array[i] != 0:
+        for _ in range(s_array[i]):
             print(i)
-
