08568e5fbf630f43792d4c9352bcea869022e4b0
noeul <kee12355@gmail.com>
2025-03-19 14:48:53 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -2,24 +2,19 @@ import sys
 
 n = int(sys.stdin.readline())
 
-numbers = list(map(int,sys.stdin.readline().split()))
+numbers = list(map(int, sys.stdin.readline().split()))
+
 cnt = 0
 
-for number in numbers:
-    if number < 2:
-        continue
-
-    is_prime = True
-
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
-
-    
-    if is_prime:
+for num in numbers:
+    if num == 2:
         cnt +=1
-
+        continue
+    if num != 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+            if i == num -1:
+                cnt+=1    
 
 print(cnt)
-
