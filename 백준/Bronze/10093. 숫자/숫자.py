8a888d1128372d891e3fa08a5fd23d1eb1f8f98d
noeul <kee12355@gmail.com>
2025-03-19 09:59:31 +0900

    [Bronze II] Title: 숫자, Time: 88 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.py
+++ 백준/Bronze/10093. 숫자/숫자.py
@@ -1,29 +1,18 @@
 import sys
 
-numbers = [i for i in range(1,1001)]
-
-
-
 a, b = (list(map(int,sys.stdin.readline().split())))
-
-
 
 
 if b > a:
     print(b-a-1)
+    for i in range(a+1, b):
+        print(i, end=' ')
 
-    for i in range(a, b-1):
-        if i == b-2:
-            print(numbers[i])
-        else:
-            print(f"{numbers[i]} ", end='')
+
 elif a > b:
     print(a-b-1)
+    for i in range(b+1, a):
+        print(i, end=' ')
 
-    for i in range(b, a-1):
-        if i == a-2:
-            print(numbers[i])
-        else:
-            print(f"{numbers[i]} ", end='')
 else:
     print(0)
