a642453a84db71bc80cd467878ce0114d4a3e737
noeul <kee12355@gmail.com>
2025-03-26 22:46:01 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,22 +1,59 @@
 import sys
 
+
+# reps = int(sys.stdin.readline().strip())
+
+# for i in range(reps):
+#     strr = list(sys.stdin.readline().strip())
+
+#     status = True
+
+#     while status:
+#         if len(strr) == 0:
+#             status = True
+#             break
+#         elif len(strr) < 2:
+#             status = False
+#             break
+#         for i in range (len(strr)-1):
+#             if strr[i] == '(' and strr[i+1] == ')':
+#                 del strr[i:i+2]
+#                 status = True
+#                 break
+#             else:
+#                 status = False
+
+
+#     if status:
+#         print('YES')
+#     else:
+#         print('NO')
+
+
 reps = int(sys.stdin.readline().strip())
 
+
 for i in range(reps):
-    strr = sys.stdin.readline().strip()
-    stack = []
-    status = True
-    for char in strr:
-        if char == '(':
-            stack.append(char)
-        elif char == ')':
-            if stack:
-                stack.pop()
+    stk = []
+    brk = list(sys.stdin.readline().strip())
+
+    for j in brk:
+        if j == '(':
+            stk.append(j)
+        else:
+            if stk and stk[-1] == '(':
+                stk.pop()
             else:
-                status = False
-                break
-    if status and not stack:
-        print('YES')
-    else:
-        print('NO')
+                stk.append(j)
     
+
+    if stk:
+        print('NO')
+    else:
+        print('YES')
+
+
+
+        
+
+
