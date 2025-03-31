f963d492bc8a8bf40c3cd0c6bff4f4589da23be3
noeul <kee12355@gmail.com>
2025-03-31 11:21:53 +0900

    [Gold IV] Title: 이분 그래프, Time: 1332 ms, Memory: 55208 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,5 +1,6 @@
 import sys
 sys.setrecursionlimit(10**6)
+
 # 어떤 정점이 있을 때 인접한 정점과 다른 색으로 칠해져있어야 함.
 # 나와 연결된 노드에 나와 다른 색을 칠해라.
 
@@ -15,30 +16,36 @@ def dfs(v, col):
         elif col[nxt] == col[v]:
             return False
     return True
+            
+
+
 
 for _ in range(n):
     m , k = list(map(int,sys.stdin.readline().split()))
-
-    col = [-1] * (m+1)
-    graph = [[] for i in range(m+1)]
-
     check = True
+    graph = [[] for _ in range(m+1)]
+    col = [-1] * (m+1)
 
     for _ in range(k):
-        a , b = list(map(int,sys.stdin.readline().split()))
+        a, b = list(map(int, sys.stdin.readline().split()))
         graph[a].append(b)
         graph[b].append(a)
 
-    for i in range(1,m+1):
+    for i in range(1, m+1):
         if col[i] == -1:
             col[i] = 0
             if not dfs(i, col):
                 check = False
+                break
+
 
     if check:
         print("YES")
     else:
         print("NO")
+
+
+
 
 
 
