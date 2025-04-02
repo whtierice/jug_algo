6b68af888fb3228b18e2905ddb007247bbbc5b5c
noeul <kee12355@gmail.com>
2025-04-02 11:18:12 +0900

    [Gold IV] Title: 이분 그래프, Time: 1308 ms, Memory: 53912 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,48 +1,56 @@
 import sys
 sys.setrecursionlimit(10**6)
 
+# 노드마다 처음 방문할 때 색상 부여
 # 어떤 정점이 있을 때 인접한 정점과 다른 색으로 칠해져있어야 함.
 # 나와 연결된 노드에 나와 다른 색을 칠해라.
 
-n = int(sys.stdin.readline().strip())
+x = int(sys.stdin.readline().strip())
 
-
-def dfs(v, col):
+        
+def dfs(v):
     for nxt in graph[v]:
         if col[nxt] == -1:
             col[nxt] = 1 - col[v]
-            if not dfs(nxt, col):
+            if not dfs(nxt):
                 return False
-        elif col[nxt] == col[v]:
-            return False
+        else:
+            if col[nxt] == col[v]:
+                return False
     return True
             
 
-
-
-for _ in range(n):
-    m , k = list(map(int,sys.stdin.readline().split()))
+for _ in range(x):
+    a, b = list(map(int, sys.stdin.readline().split()))
+    
+    graph = [[] for _ in range(a+1)]
+    col = [-1] * (a+1)
     check = True
-    graph = [[] for _ in range(m+1)]
-    col = [-1] * (m+1)
 
-    for _ in range(k):
-        a, b = list(map(int, sys.stdin.readline().split()))
-        graph[a].append(b)
-        graph[b].append(a)
+    for _ in range(b):
+        q, w = list(map(int, sys.stdin.readline().split()))
+        graph[q].append(w)
+        graph[w].append(q)
 
-    for i in range(1, m+1):
+    for i in range(0,a+1):
         if col[i] == -1:
             col[i] = 0
-            if not dfs(i, col):
+            if not dfs(i):
                 check = False
                 break
-
-
+    
     if check:
-        print("YES")
+        print('YES')
     else:
-        print("NO")
+        print('NO')
+
+
+
+
+
+
+
+
 
 
 
