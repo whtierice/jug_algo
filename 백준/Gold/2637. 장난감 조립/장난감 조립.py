68c3214c87bfb52752aecd09871bd34fd1ab3ad9
noeul <kee12355@gmail.com>
2025-04-02 19:53:34 +0900

    [Gold II] Title: 장난감 조립, Time: 56 ms, Memory: 34976 KB -BaekjoonHub

---
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().strip())
+m = int(sys.stdin.readline().strip())
+
+graph = [[] for _ in range(n+1)]
+indi = [0] * (n+1) 
+jng = [[0] * (n + 1) for _ in range(n+1)]
+q = deque()
+
+for _ in range(m):
+    a, b, c = list(map(int, sys.stdin.readline().split()))
+    graph[b].append((a,c))
+    indi[a] +=1
+
+for i in range(1,n+1):
+    if indi[i] == 0:
+        jng[i][i] = 1
+        q.append(i)
+
+while q:
+    cur = q.popleft()
+    for nxt, cost in graph[cur]:
+        indi[nxt] -=1
+        if indi[nxt] == 0:
+            q.append(nxt)
+        for i in range(1,n+1):
+            jng[nxt][i] = jng[nxt][i] + (jng[cur][i] * cost)
+                
+                
+                    
+            
+for i in range(1,n+1):
+    if jng[n][i]:
+        print(f'{i} {jng[n][i]}')
+
+
