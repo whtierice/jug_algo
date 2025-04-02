558c2f7abbd3315caf7f6409aeb78717f857c77f
noeul <kee12355@gmail.com>
2025-04-03 01:06:43 +0900

    [Gold II] Title: 장난감 조립, Time: 56 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.py
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -5,12 +5,11 @@ n = int(sys.stdin.readline().strip())
 m = int(sys.stdin.readline().strip())
 
 graph = [[] for _ in range(n+1)]
-indi = [0] * (n+1) 
-jng = [[0] * (n + 1) for _ in range(n+1)]
+indi = [0] * (n+1)
+jng = [[0] * (n+1) for _ in range(n+1)]
 q = deque()
-
 for _ in range(m):
-    a, b, c = list(map(int, sys.stdin.readline().split()))
+    a , b, c = list(map(int,sys.stdin.readline().split()))
     graph[b].append((a,c))
     indi[a] +=1
 
@@ -22,17 +21,12 @@ for i in range(1,n+1):
 while q:
     cur = q.popleft()
     for nxt, cost in graph[cur]:
+        for i in range(1,n+1):
+            jng[nxt][i] += (jng[cur][i] * cost)
         indi[nxt] -=1
         if indi[nxt] == 0:
             q.append(nxt)
-        for i in range(1,n+1):
-            jng[nxt][i] = jng[nxt][i] + (jng[cur][i] * cost)
-                
-                
-                    
-            
-for i in range(1,n+1):
-    if jng[n][i]:
-        print(f'{i} {jng[n][i]}')
 
-
+for i, j in enumerate(jng[n]):
+    if j != 0:
+        print(f"{i} {j}")
