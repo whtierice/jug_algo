ff9943d7afadc9bc3304ba91646015ede4f81656
noeul <kee12355@gmail.com>
2025-06-15 22:44:27 +0900

    [Gold IV] Title: 인구 이동, Time: 652 ms, Memory: 116640 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,48 +1,62 @@
-import math
 import sys
 from collections import deque
+n, l, r = list(map(int, sys.stdin.readline().split()))
 
-N, L, R = map(int,sys.stdin.readline().split())
-population = []
-queue = deque()
-for _ in range(N):
-    population.append(list(map(int,sys.stdin.readline().split())))
-dx = [-1,1,0,0]
-dy = [0,0,1,-1]
+a = []
+for _ in range(n):
+    a.append(list(map(int,sys.stdin.readline().split())))
+
+
+
+# dir = [(1,0),(0,1),(-1,0),(0,-1)]
+# 튜플 언패킹 보단 정수 인덱스를 사용하도록 하는 게 시간 복잡도에서 효율적
+
+dx = (-1,1,0,0)
+dy = (0,0,1,-1)
+q = deque()
 day = 0
 
-while(True):
-    check = False
-    visited = [[False]*N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            if not visited[i][j]:
-                visited[i][j] = True
-                union = []
-                queue.append((i,j))
-                cnt = 0
-                total = 0
-                while queue:
-                    x, y = queue.popleft()
-                    union.append((x,y))
-                    cnt += 1
-                    total += population[x][y]
-                    for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
-                        if 0<= nx < N and 0<= ny < N and not visited[nx][ny]:
-                            if L <= abs(population[x][y] - population[nx][ny]) <=R:
-                                queue.append((nx,ny))
-                                visited[nx][ny] = True                
-                        
-                if cnt != 1:
-                    for x,y in union:
-                        avg = math.floor(total/cnt)
-                        population[x][y] = avg
-                        check = True
-    if check == False:
-        break
-    else:
-        day += 1
+while True:
+    visited = [[False]*n for _ in range(n)]
+    moved = False
 
+    
+    for rr in range(n):
+        for c in range(n):
+            if not visited[rr][c] :
+                q.append((rr,c))
+                visited[rr][c] = True
+                mem = [(rr,c)]
+                total_pop = a[rr][c]
+
+                while q:
+                    x, y = q.popleft()
+                    for i in range(4):
+                        nx , ny = x + dx[i], y +dy[i]
+
+                        if 0<=nx < n and 0<=ny < n and not visited[nx][ny]:
+                            if l <= abs(a[x][y] - a[nx][ny]) <= r:
+                                visited[nx][ny] = True
+                                q.append((nx,ny))
+                                mem.append((nx,ny))
+                                total_pop += a[nx][ny]
+
+
+                                
+            
+            if (len(mem) >= 2):
+                avg_pop = total_pop // len(mem)
+                moved = True
+                for (o,p) in mem:
+                    a[o][p] = avg_pop
+
+    
+    if not moved:
+        break
+    day += 1
 print(day)
+
+
+
+
+
