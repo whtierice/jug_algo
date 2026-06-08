fd0bd05e8c504d830c5199dd9a148e81ca16e74f
noeul <kee12355@gmail.com>
2026-06-08 21:36:00 +0900

    [level 3] Title: 억억단을 외우자, Time: 3441.56 ms, Memory: 93.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.py
@@ -0,0 +1,22 @@
+def solution(e, starts):
+    
+    divisor_count = [0] * (e + 1)
+    for i in range(1, e + 1):
+        for j in range(i, e + 1, i):
+            divisor_count[j] += 1
+            
+    suffix_max = [0] * (e + 2)
+    suffix_max[e] = e  # e~e 구간의 답은 e 자신
+
+    for i in range(e - 1, 0, -1):
+        if divisor_count[i] >= divisor_count[suffix_max[i + 1]]:
+            suffix_max[i] = i
+        else:
+            suffix_max[i] = suffix_max[i + 1]
+                
+    result = []
+    
+    for i in starts:
+        result.append(suffix_max[i])
+        
+    return result
