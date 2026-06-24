b2a34e28d162e435c577edc5acd164ac847d5ef0
noeul <kee12355@gmail.com>
2026-06-24 21:52:27 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,15 @@
 def solution(clothes):
     answer = 1
     
-    cloth_counts = {}
+    counter = {}
     
-    for name, category in clothes:
-        cloth_counts[category] = cloth_counts.get(category, 0) + 1
-        
-    for count in cloth_counts.values():
+    for names, category in clothes:
+        counter[category] = counter.get(category, 0) + 1
+    
+    print(counter)
+    
+    for count in counter.values():
         answer *= count + 1
         
     return answer - 1
+        
